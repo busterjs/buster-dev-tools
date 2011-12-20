005516f8ed11970f45d64f37aece796c3e4eecb9
August Lilleaas <august@augustl.com>
2011-12-20 18:03:01 +0100

    Renaming call to none existing function.

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
 var projects = require("./project-list");
 var f = require("./functions");
 
-f.withProjects(projects, [f.updateProject, f.symlinkProjectDependencies, f.npmLinkProject, f.symlinkProjectDependencies, f.updateProjectSubmodules]);
+f.withProjects(projects, [f.updateProject, f.symlinkProjectDependencies, f.npmLinkProject, f.symlinkProjectDependencies, f.initProjectSubmodules]);
