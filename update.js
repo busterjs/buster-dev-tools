f543d9b185ba7de4b30fb34aa5f75c06253c4942
August Lilleaas <august@augustl.com>
2011-12-20 18:26:32 +0100

    Adding --skip-update option that will only do npm linking and symlinking.

--- update.js
+++ update.js
@@ -1,4 +1,16 @@
 var projects = require("./project-list");
 var f = require("./functions");
 
-f.withProjects(projects, [f.updateProject, f.symlinkProjectDependencies, f.npmLinkProject, f.symlinkProjectDependencies, f.updateProjectSubmodules]);
+var argv = process.argv.slice(2);
+var tasks = [];
+
+if (argv.indexOf("--skip-update") > -1) {
+    tasks.push(f.updateProject);
+}
+
+tasks.push(f.symlinkProjectDependencies);
+tasks.push(f.npmLinkProject);
+tasks.push(f.symlinkProjectDependencies);
+tasks.push(f.updateProjectSubmodules);
+
+f.withProjects(projects, tasks);
