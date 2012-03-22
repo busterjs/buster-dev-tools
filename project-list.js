ec7a040877809eb000284ff394ce3374036e4c79
Matthias Kling <m.kling@sprax.eu>
2012-03-22 16:52:26 +0100

    * moved initialization of project objects into project-list.js: a) default git url defined in same file as projects; b) need to do it only once * moved getProject(..) there as well, as initProject(project); made gitUrl a truly optional prop of a project object + comment on optional require ./local.js

--- project-list.js
+++ project-list.js
@@ -1,3 +1,10 @@
+var path  = require("path");
+var devDir = path.resolve(__dirname, "../"); // dev env is one folder up from this file, independent of current working dir
+
+function defaultGitUrl(projectName) {
+    return "https://github.com/busterjs/" + projectName + ".git";
+}
+
 var projects = [
     {name: "sinon", gitUrl: "https://github.com/cjohansen/Sinon.JS.git"},
     "buster-util",
@@ -22,11 +29,23 @@ var projects = [
     "buster-cli",
     "buster-test-cli",
     "buster-static",
-    "buster",
-//    "buster-jstestdriver",    // not really necessary; depends on buster-html-doc
-//    "buster-html-doc"         // not really necessary; depends on contextify (through jsdom) which makes problems on Win
+    "buster"
+//    ,"buster-jstestdriver"    // not really necessary; depends on buster-html-doc
+//    ,"buster-html-doc"         // not really necessary; depends on contextify (through jsdom) which makes problems on Win
 ];
 
+// Pull in additional projects listed in ./local, same format as above.
 try { projects = require("./local").concat(projects) } catch(e){};
 
-module.exports = projects;
+function initProject(project) {
+    if (typeof project == "string") {
+        project = { name: project };
+    }
+    project.gitUrl    = project.gitUrl || defaultGitUrl(project.name);
+    project.localPath = path.join(devDir, project.name);
+    return project;
+}
+
+module.exports = projects
+    .map(initProject)
+;
