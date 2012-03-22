b85f6733b008745bb889fdd6c45c97e987b89375
Matthias Kling <m.kling@sprax.eu>
2012-03-22 19:32:00 +0100

    + method skipDep(depName,depVersion) on project objects, to decide on a per (project, dependency) basis whether to skip the dep install; allows for additional action to be taken such as get it from somewhere else than thru npm

--- project-list.js
+++ project-list.js
@@ -21,7 +21,7 @@ var projects = [
     "buster-glob",
     "buster-resources",
     "buster-capture-server",
-    "buster-bayeux-emitter",
+    { name: "buster-bayeux-emitter", skipDep: skipOnWindows("faye", "blahatest") },
     "buster-configuration",
     "buster-client",
     "buster-args",
@@ -44,11 +44,24 @@ function initProject(project) {
     project.gitUrl    = project.gitUrl || defaultGitUrl(project.name);
     project.localPath = path.join(devDir, project.name);
     project.skip      = project.skip || function () { return false; }; // do not skip entire projects by default
+    project.skipDep   = project.skipDep || function() { return false; }; // install any (external) deps by default
     return project;
 }
 
 function platformIsWindows() {
     return process.platform == "win32";
+}
+
+function skipOnWindows() {
+    var toBeSkipped = Array.prototype.slice.call(arguments, 0);
+    return function (depName, depVersion) {
+        if (platformIsWindows() && toBeSkipped.some(function(d) { return d == depName; })) {
+            // could take alternative action here, e.g. getting it from somewhere else than thru npm
+            console.warn("Warning: skipped dependency " + depName + "@" + depVersion + " of " + this.name);
+            return true; // tell operation to skip it
+        }
+        return false;
+    };
 }
 
 module.exports = projects
