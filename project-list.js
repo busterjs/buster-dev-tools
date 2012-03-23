914eccfec97392124ff4aae5cfcd1cf635c1fdfa
Matthias Kling <m.kling@sprax.eu>
2012-03-23 22:23:11 +0100

    + util function installDummy, for putting in a minimal package.json in order to prevent npm from trying to install it * using it for for the contextify workaround on win (dependency of buster-html-doc, thru jsdom) * moved helper functions directoryExists and quote from buster-dev-tools.js to util.js + helper function fileExists, analogous to directoryExists

--- project-list.js
+++ project-list.js
@@ -1,8 +1,9 @@
-var path = require("path"), util = require("./lib/util");
+var path = require("path"),
+    util = require("./lib/util");
 var True = util.True, False = util.False, If = util.If,
     onWindows = util.onWindows, onMacOS= util.onMacOS, onLinux = util.onLinux,
-    itMatches = util.itMatches
-;
+    itMatches    = util.itMatches,
+    installDummy = util.installDummy;
 
 function defaultGitUrl(projectName) {
     return "https://github.com/busterjs/" + projectName + ".git";
@@ -17,7 +18,7 @@ var projects = [
         // Here's a more complex workaround: on Windows, jsdom fails because npm can't install it's dependency contextify
         // So what we do is put in a dummy contextify s.t. npm will not try to install it
         // Note that we do NOT entirely skip the installation of jsdom
-        ,skipDep: If(onWindows).And(itMatches, "jsdom").Then(util.dummyAction, "contextify@0.1.1")
+        ,skipDep: If(onWindows).And(itMatches, "jsdom").Then(installDummy, "contextify@0.1.1")
     },
     { name: "buster-docs", skip: True },  // just for demo, entire project will be skipped
     { name: "sinon", gitUrl: "https://github.com/cjohansen/Sinon.JS.git" },
