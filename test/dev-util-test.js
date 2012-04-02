52d407a2ab2c4404b248b9a05f085b324a18e628
Matthias Kling <m.kling@sprax.eu>
2012-04-02 15:11:08 +0200

    + tests for installNpmDummy, directoryExists and fileExists

--- test/dev-util-test.js
+++ test/dev-util-test.js
@@ -3,6 +3,7 @@ var assert = buster.assert;
 var refute = buster.refute;
 
 var fs = require("fs");
+var path = require("path");
 
 var du = require("../lib/dev-util");
 var quote               = du.quote,
@@ -38,10 +39,8 @@ testCase("dev-utils", {
             refute(directoryExists(__filename));
         },
 
-        "//returns false on non-existent directory": function() {
-        },
-
-        "//returns false on non-existent file": function() {
+        "returns false on non-existent directory or file": function() {
+            refute(directoryExists(path.join(__dirname, "i", "should", "not", "exist")));
         },
 
     },
@@ -56,10 +55,8 @@ testCase("dev-utils", {
             assert(fileExists(__filename));
         },
 
-        "//returns false on non-existent directory": function() {
-        },
-
-        "//returns false on non-existent file": function() {
+        "returns false on non-existent file or directory": function() {
+            refute(fileExists(path.join(__dirname, "i", "should", "not", "exist")));
         },
 
     },
@@ -73,7 +70,69 @@ testCase("dev-utils", {
 
     "installNpmDummy": {
 
-        "//has expected side effect in file system": function() {
+        setUp: function() {
+            this.projectDummy = { name: "dummy-project", localPath: path.join(__dirname, "fixtures") };
+            this.dep = "foo";
+            this.nonExistingSubDep = "bar";
+            this.existingSubDep    = "baz";
+            var nodeModules = path.join(this.projectDummy.localPath, "node_modules");
+            this.nonExistingPackageFile = path.join(nodeModules, this.nonExistingSubDep, "package.json");
+            this.existingPackageFile    = path.join(nodeModules, this.existingSubDep, "package.json");
+            
+            this.existingPackageFileContents = "asefdio{[%]}uzviubzizrene 7774$";
+
+            this.purgeFixtures = function() {
+                if (directoryExists(nodeModules)) { // delete node_modules and contents
+                    if (directoryExists(path.join(nodeModules, this.existingSubDep))) {
+                        if (fileExists(this.existingPackageFile)) {
+                            fs.unlinkSync(this.existingPackageFile);
+                        }
+                        fs.rmdirSync(path.join(nodeModules, this.existingSubDep));
+                    }
+                    if (directoryExists(path.join(nodeModules, this.nonExistingSubDep))) {
+                        if (fileExists(this.nonExistingPackageFile)) {
+                            fs.unlinkSync(this.nonExistingPackageFile);
+                        }
+                        fs.rmdirSync(path.join(nodeModules, this.nonExistingSubDep));
+                    }
+                    fs.rmdirSync(nodeModules);
+                }
+            };
+
+            // make sure projectDummy.localPath exists
+            if (!directoryExists(this.projectDummy.localPath)) {
+                fs.mkdirSync(this.projectDummy.localPath);
+            }
+
+            // start from clean
+            this.purgeFixtures();
+
+            // create "existing" package file
+            fs.mkdirSync(nodeModules);
+            fs.mkdirSync(path.join(nodeModules, this.existingSubDep));
+            fs.writeFileSync(this.existingPackageFile, this.existingPackageFileContents);
+        },
+
+        tearDown: function() {
+            this.purgeFixtures();
+        },
+
+        "creates valid package.json at correct location if it's not yet there": function() {
+            installNpmDummy.call(this.projectDummy, this.nonExistingSubDep, this.dep);
+            assert(fileExists(this.nonExistingPackageFile), this.nonExistingPackageFile + " should exist");
+
+            var contents = require(this.nonExistingPackageFile);
+            assert.equals(contents.name, this.nonExistingSubDep, "name field in package.json");
+            refute.equals(contents.version, "", "version field in package.json non-empty");
+        },
+
+        "leaves existing package.json untouched": function() {
+            installNpmDummy.call(this.projectDummy, this.existingSubDep, this.dep);
+            assert(fileExists(this.existingPackageFile), this.existingPackageFile + " should exist");
+
+            var contents = fs.readFileSync(this.existingPackageFile);
+            assert.equals(contents, this.existingPackageFileContents, "contents of existent file");
+
         },
 
     },
